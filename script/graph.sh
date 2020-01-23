e3dc67893624d542bb5402969244bc3835c9bd10
Bob marley <bobmarley@localhost.localdomain>
2020-01-23 18:28:52 +0100

    I wish it will be the final version

--- script/graph.sh
+++ script/graph.sh
@@ -2,10 +2,10 @@
 #gnuplot
 set xrange [0:20]
 set yrange [0:400]
-plot [:] [0:8000] "file.dat" title "bonjour tout le monde" linetype 7 linecolor 0 with linespoints
+system 
+plot [:] [0:system(" echo $(( $(grep MemTotal: /proc/meminfo | grep -Eo '[0-9]{1,10}') / 1024 )) ")] "file.dat" title "RAM evolution" linetype 7 linecolor 0 with linespoints
 pause 1
 reread
 set autoscale
 #pause -1
 #quit
-
